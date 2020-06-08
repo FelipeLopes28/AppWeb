ec206f358f9e0cb85693f6f015d374fe59495188
FelipeLopes28 <felipelopes182640@sou.urcamp.edu.br>
2020-06-08 15:53:31 -0300

    add comentarios

--- Calculadora/js/script.js
+++ Calculadora/js/script.js
@@ -1,16 +1,18 @@
-  // LIMPAR RESULTADOS
-function C(){ 
-  document.caixas.caixa1.value = "";
-  document.caixas.caixa2.value = "";
-  document.querySelector('.resultado').innerHTML= "";
-}
-
-  // DIVISÃO
-function Divisao(){
+   // SOMA
+function Soma(){
   var caixa1 = document.querySelector('.caixa1').value;
   var caixa2 = document.querySelector('.caixa2').value;
 
-  var resultado = parseInt(caixa1) / parseInt(caixa2);
+  var resultado = parseInt(caixa1) + parseInt(caixa2);
+  document.querySelector('.resultado').innerHTML= resultado;
+}
+
+  // SUBTRAÇÃO
+function Subtracao(){
+  var caixa1 = document.querySelector('.caixa1').value;
+  var caixa2 = document.querySelector('.caixa2').value;
+
+  var resultado = parseInt(caixa1) - parseInt(caixa2);
   document.querySelector('.resultado').innerHTML= resultado;
 }
 
@@ -22,23 +24,20 @@ function Multiplicacao(){
   var resultado = parseInt(caixa1) * parseInt(caixa2);
   document.querySelector('.resultado').innerHTML= resultado;
 }
-
-// SOMA
-function Soma(){
+  // DIVISÃO
+function Divisao(){
   var caixa1 = document.querySelector('.caixa1').value;
   var caixa2 = document.querySelector('.caixa2').value;
 
-  var resultado = parseInt(caixa1) + parseInt(caixa2);
+  var resultado = parseInt(caixa1) / parseInt(caixa2);
   document.querySelector('.resultado').innerHTML= resultado;
 }
 
-// SUBTRAÇÃO
-function Subtracao(){
-  var caixa1 = document.querySelector('.caixa1').value;
-  var caixa2 = document.querySelector('.caixa2').value;
-
-  var resultado = parseInt(caixa1) - parseInt(caixa2);
-  document.querySelector('.resultado').innerHTML= resultado;
+  // LIMPAR RESULTADOS
+function C(){ 
+  document.caixas.caixa1.value = "";
+  document.caixas.caixa2.value = "";
+  document.querySelector('.resultado').innerHTML= "";
 }
 
 
